88cb51aa799858f2e2b8f8531548140ae8fa2df8
biruk1122 <birukfikadu29@gmail.com>
2024-06-20 09:27:23 -0700

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
+""" Contains makeChange function"""
+
+
 def makeChange(coins, total):
-    if total < 0:
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
         return 0
-    if total == 0:
-        return 0
-    
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
